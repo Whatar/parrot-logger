63087223817b7474214a5ceead917ae7165e8535
Riccardo Zanotelli <riccardo.zanotelli@gmail.com>
2024-10-10 12:55:34 +0200

    Refactor logger configuration

--- src/utils/env.utils.ts
+++ src/utils/env.utils.ts
@@ -1,18 +1,26 @@
 // Models
 import { Options } from '../model/options.model';
+import * as dotenv from 'dotenv';
 
-/** Load the options from the environment variables */
-export function uploadOptionsToEnv(options: Options): void {
-  if (options.hideLog !== undefined) process.env.LORIKEET_HIDE_LOG = options.hideLog.toString();
-  if (options.emoji !== undefined) process.env.LORIKEET_EMOJI = options.emoji.toString();
-  if (options.emoji !== undefined) process.env.LORIKEET_SEPARATOR = options.separator;
-}
+// Load the evirnoment variables
+dotenv.config();
 
-/** Get the options from the environment variables, here we set the default values */
-export function getOptionsFromEnv(): Options {
-  return {
-    hideLog: process.env.LORIKEET_HIDE_LOG !== undefined ? process.env.HIDE_LOG === 'true' : false,
-    emoji: process.env.LORIKEET_EMOJI !== undefined ? process.env.EMOJI === 'true' : true,
+/** Get the options from the environment variables if they exist or use the default values */
+export function getOptions(): Options {
+  const options = {
+    hideLog: process.env.LORIKEET_HIDE_LOG === 'true' || false,
+    emoji: process.env.LORIKEET_EMOJI === 'true' || true,
     separator: process.env.LORIKEET_SEPARATOR || ' '
   };
+
+  return options;
+}
+
+/** Load the options from the environment variables */
+export function updateOptions(options: Options): Options {
+  if (options.hideLog !== undefined) process.env.LORIKEET_HIDE_LOG = String(options.hideLog);
+  if (options.emoji !== undefined) process.env.LORIKEET_EMOJI = String(options.emoji);
+  if (options.separator !== undefined) process.env.LORIKEET_SEPARATOR = options.separator;
+
+  return getOptions();
 }
